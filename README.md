192122ad40eddde97a0eb3f354c09e0e0b70c1fb
Matt Dexter <matt@mattdexter.io>
2017-11-06 20:01:23 -0500

    Working on docker deploy process

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Dockerized Django server
 This is the server that hosts my Django apps.
+
+Enter your environment variables in .env.tmpl and rename it to .env
--- webapp/mattdexter/settings.py
+++ webapp/mattdexter/settings.py
@@ -25,7 +25,8 @@ SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ['VIRTUAL_HOST'],
+                 "www." + os.environ['VIRTUAL_HOST']]
 
 
 # Application definition
