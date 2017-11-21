9ac55e0fa3f5185fffcd4ba3c931fdb4a48e4e3e
Matt Dexter <matt@mattdexter.io>
2017-11-21 17:06:54 -0500

    Switched to ViewSets

--- webapp/mattdexter/settings.py
+++ webapp/mattdexter/settings.py
@@ -33,13 +33,13 @@ ALLOWED_HOSTS = [os.environ['VIRTUAL_HOST'],
 
 INSTALLED_APPS = [
     'portfolio.apps.PortfolioConfig',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
 ]
 
 MIDDLEWARE = [
